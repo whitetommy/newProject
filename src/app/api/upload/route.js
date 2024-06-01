454f235902de1490d36f16eb602c2c095fd6f0f3
whitetommy <1228tommy@naver.com>
2024-06-01 07:17:24 +0000

    proxy_test12

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -3,7 +3,7 @@ import http from 'http';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL.replace,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   httpAgent: new http.Agent({
     rejectUnauthorized: false, 
   }),
