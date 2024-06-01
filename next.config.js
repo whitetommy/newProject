d29166fbb1adf0b6e281246934686d6b25ecb5c4
whitetommy <1228tommy@naver.com>
2024-06-01 04:25:02 +0000

    fix/proxy_test1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/upload",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/upload`,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -57,7 +57,7 @@ const MyPage = () => {
       date: new Date().toISOString().split('T')[0],
       visibility: selectedVisibility
     };
-
+    console.log(session);
     console.log(session.user.id);
 
     const formData = new FormData();
@@ -66,7 +66,7 @@ const MyPage = () => {
     formData.append('visibility', selectedVisibility);
 
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/upload`, formData, {
+      const response = await axios.post(`/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
